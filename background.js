dba52a59f61b5236f42db8e642f0ff3820fba716
ken1flan <ken1flan@gmail.com>
2021-01-03 19:02:22 +0900

    Shorten url
    amazonの商品URLの場合に、付属している不要な情報を削除します。

--- background.js
+++ background.js
@@ -2,10 +2,12 @@ chrome.browserAction.onClicked.addListener(() => {
   chrome.tabs.query({active: true, currentWindow: true}, tabs => {
     url = tabs[0].url;
 
+    shorted_url = url.replace(/(https:\/\/www.amazon.co.jp).*(\/dp\/[^/]+).*/, "$1$2");
+
     // clipboardへコピー
     var textArea = document.createElement("textarea");
     document.body.appendChild(textArea);
-    textArea.value = url;
+    textArea.value = shorted_url;
     textArea.select();
     document.execCommand("copy");
     document.body.removeChild(textArea);
