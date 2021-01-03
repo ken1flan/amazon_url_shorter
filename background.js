c2c883499cc7cf0504ab5d8ac83cd86d967017e7
ken1flan <ken1flan@gmail.com>
2021-01-03 15:53:57 +0900

    Copy to clipboard

--- background.js
+++ background.js
@@ -1,6 +1,13 @@
 chrome.browserAction.onClicked.addListener(() => {
   chrome.tabs.query({active: true, currentWindow: true}, tabs => {
     url = tabs[0].url;
-    console.log(url);
+
+    // clipboardへコピー
+    var textArea = document.createElement("textarea");
+    document.body.appendChild(textArea);
+    textArea.value = url;
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
   });
 });
